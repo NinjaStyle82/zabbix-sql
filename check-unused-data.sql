2fa1c5d27ae1bf42ce5661c2fbd29c764d7d3f91
Pavel N. Savenkov <xeenon@me.com>
2015-01-01 19:35:49 +0300

    huge speedup for check-unused

--- check-unused-data.sql
+++ check-unused-data.sql
@@ -1,8 +1,51 @@
-SELECT count(itemid) AS history FROM history WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
-SELECT count(itemid) AS history_uint FROM history_uint WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
-SELECT count(itemid) AS history_str FROM history_str WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
-SELECT count(itemid) AS history_text FROM history_text WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
-SELECT count(itemid) AS history_log FROM history_log WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+DELIMITER $$
+DROP PROCEDURE IF EXISTS __count_unused_items $$
+CREATE PROCEDURE __count_unused_items(
+    tableName VARCHAR(32)
+)
+BEGIN
 
-SELECT count(itemid) AS trends FROM trends WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
-SELECT count(itemid) AS trends_uint FROM trends_uint WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+    SET @qry = concat('
+    	drop temporary table if exists __uitems_', tableName ,'
+    ');
+    PREPARE `qry` FROM @qry; EXECUTE `qry`; DEALLOCATE PREPARE `qry`;
+
+    SET @qry = concat('
+    	create temporary table __uitems_', tableName ,' (
+    	    itemid bigint UNSIGNED primary key
+    	) engine=memory
+                select distinct `itemid` from ', tableName ,'
+    ');
+    PREPARE `qry` FROM @qry; EXECUTE `qry`; DEALLOCATE PREPARE `qry`;
+
+    SET @qry = concat('
+        delete from __uitems_', tableName ,' where itemid in (select itemid from __good_items);
+    ');
+    PREPARE `qry` FROM @qry; EXECUTE `qry`; DEALLOCATE PREPARE `qry`;
+
+    SET @qry = concat('
+    	SELECT count(i.itemid) AS ', tableName ,' FROM ', tableName ,' INNER JOIN __uitems_', tableName ,' i USING (itemid)
+    ');
+    PREPARE `qry` FROM @qry; EXECUTE `qry`; DEALLOCATE PREPARE `qry`;
+
+END
+$$
+
+delimiter ;
+
+DROP TEMPORARY TABLE IF EXISTS __good_items;
+CREATE TEMPORARY TABLE __good_items ENGINE=MEMORY
+        SELECT itemid FROM items WHERE status='0'
+;
+ALTER TABLE __good_items ADD PRIMARY KEY (itemid);
+
+CALL __count_unused_items('history');
+CALL __count_unused_items('__history_uitems');
+CALL __count_unused_items('history_str');
+CALL __count_unused_items('history_text');
+CALL __count_unused_items('history_log');
+
+CALL __count_unused_items('trends');
+CALL __count_unused_items('trends_uint');
+
+DROP PROCEDURE IF EXISTS __count_unused_items;
